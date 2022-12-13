5aa6bb38a80b448287abc9bfe901c8ced8ee91ae
rkhademul9 <khademul@flyfarint.com>
2022-12-13 13:00:39 +0600

    TravellerModuleAdded

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthProvider from "./components/Contexts/AuthProvider";
 import Traveller from "./components/TravelerModule/Traveller";
+import AddTraveller from "./components/TravelerModule/AddTraveller";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       {/* all route */}
       <AuthProvider>
         <BrowserRouter>
           <Routes>
             <Route path="/traveler" element={<Traveller />} />
+            <Route path="/addtraveler" element={<AddTraveller />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
